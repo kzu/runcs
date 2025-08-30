6d297e2baed14d19af68c18ead2e811153a53b0d
Daniel Cazzulino <daniel@cazzulino.com>
2025-08-29 22:49:17 -0300

    Integrate Spectre.Console for enhanced output

--- run.cs
+++ run.cs
@@ -1,1 +1,5 @@
-System.Console.WriteLine("run.cs");
+#:package Spectre.Console@*
+
+using Spectre.Console;
+
+AnsiConsole.MarkupLine($"Hello world from [green]dnx[/] [yellow]gist[/] :rocket: [bold italic]{string.Join(' ', args)}[/]");
